96d99cbda1bdd0ca00f1769b662558bc2840b61e
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2020-11-17 18:04:31 +0700

    Merge and add styles for compose

--- server/modules/uploadImage/controller/uploadImage.api.js
+++ server/modules/uploadImage/controller/uploadImage.api.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const upload = require("../uploadMiddleware");
 
 router.post("/post-image", (req, res) => {
-  console.log("UPLOAD", req.file, req.body, req);
-  upload(req, res, function (err) {
-    if (err) {
-      console.log(err);
-      return res.send(err);
-    }
-    // Display uploaded image for user validation
-    console.log(req.file);
-    return res.json({
-      url: "",
-    });
-  });
+  console.log("UPLOAD", req.files, req.body, req);
+  // upload(req, res, function (err) {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.send(err);
+  //   }
+  //   // Display uploaded image for user validation
+  //   console.log(req.file);
+  //   return res.json({
+  //     url: "",
+  //   });
+  // });
 });
 
 module.exports = router;
--- server/modules/uploadImage/uploadMiddleware.js
+++ server/modules/uploadImage/uploadMiddleware.js
@@ -10,10 +10,9 @@ const imageFilter = function (req, file, cb) {
 };
 
 const storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  //   cb(null, path.join(__dirname, "../../", "../public/upload"));
-  // },
-  destination: "public/upload",
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../", "../public/upload"));
+  },
   filename: function (req, file, cb) {
     cb(
       null,
