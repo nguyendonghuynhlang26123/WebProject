c5dbee8fc8152e8f5deea379c017bb3d0b207a66
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2020-11-29 23:46:23 +0700

    Adjusting front end

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,33 @@
 window.addEventListener("load", () => {
-  document.querySelectorAll(".glider").forEach((e) => {
-    new Glider(e, {
-      // Mobile-first defaults
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      scrollLock: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 3,
-            slidesToScroll: "auto",
-          },
+  new Glider(document.querySelector(".glider1"), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: ".dots",
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 3,
+          slidesToScroll: "auto",
         },
-        {
-          // screens greater than >= 1024px
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: "auto",
-            itemWidth: 400,
-            duration: 0.25,
-          },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: "auto",
+          itemWidth: 400,
+          duration: 0.25,
         },
-      ],
-    });
+      },
+    ],
   });
 });
--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -7,3 +7,15 @@ document.getElementById("sidenav_bg").addEventListener("click", (ev) => {
   document.getElementById("sidenav").classList.remove("active");
   document.getElementById("sidenav").classList.add("inactive");
 });
+
+let prevScrollpos = window.pageYOffset;
+document.getElementById("functional-nav").style = "position: sticky";
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("functional-nav").style.top = "0";
+  } else {
+    document.getElementById("functional-nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
