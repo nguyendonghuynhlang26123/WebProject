a9164135a04550cdb5e996a0332ef4533b6ca3fa
nguyenthaitan <nguyenthaitan9@gmail.com>
2020-12-23 07:45:38 +0700

    fix timezone schedule

--- server/modules/sendMail/sendMail.service.js
+++ server/modules/sendMail/sendMail.service.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const CronJob = require('cron').CronJob;
+const CronJob = require("cron").CronJob;
 const postService = require("../posts/services/post.service");
 
 const transporter = nodemailer.createTransport({
@@ -10,21 +10,29 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const schedule = new CronJob('0 30 7 * * *', async function () {
-  const content = await postService.getAllNewPost();
-  const mailOptions = {
-    from: "thependailynews@gmail.com",
-    to: "nguyenthaitan9@gmail.com, nguyendonghuynhlang@gmail.com",
-    subject: `New news posted in the day - ${new Date(Date.now()).toLocaleDateString()}`,
-    html: content,
-  };
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-});
+const schedule = new CronJob(
+  "0 0 8 * * *",
+  async function () {
+    const content = await postService.getAllNewPost();
+    const mailOptions = {
+      from: "thependailynews@gmail.com",
+      to: "nguyenthaitan9@gmail.com, nguyendonghuynhlang@gmail.com",
+      subject: `New news posted in the day - ${new Date(
+        Date.now()
+      ).toLocaleDateString()}`,
+      html: content,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  },
+  null,
+  false,
+  "Asia/Ho_Chi_Minh"
+);
 
 module.exports = schedule;
