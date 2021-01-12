3393897c911258299f61e9399c2905562097bf9c
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2021-01-12 12:30:03 +0700

    Add tag page

--- server/modules/tags/controller/tag.api.js
+++ server/modules/tags/controller/tag.api.js
@@ -1,31 +1,32 @@
-const tagService = require("../services/tag.service");
-const postService = require("../../posts/services/post.service");
-const express = require("express");
+const tagService = require('../services/tag.service');
+const postService = require('../../posts/services/post.service');
+const categoryService = require('../../categories/services/category.service');
+const express = require('express');
 const router = express.Router();
 
-router.get("/:tagName", async function (req, res, next) {
+router.get('/:tagName', async function (req, res, next) {
   try {
     const posts = await postService.getAllPost(
       {
         post_tags: req.params.tagName,
-        post_status: "Publish",
+        post_status: 'Publish',
       },
       req.query.select,
       Number(req.query.limit),
       null
     );
-    res.send(posts);
+    //res.send(posts);
     // TO DO: Render Tag
-    //   res.render('categoryPages/categoryPage', {
-    //     category: category,
-    //     posts: posts,
-    //   });
+    res.render('tagPages/tagPage', {
+      posts: posts,
+      tag: req.params.tagName,
+    });
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", async function (req, res) {
+router.post('/', async function (req, res) {
   const tag = await tagService.createTag(req.body.tag_name);
   res.send(tag);
 });
